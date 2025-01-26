de4eee835bf470173978206e05f9f969a7339931
Awolad Hossain <awoladh04@gmail.com>
2025-01-26 14:30:23 +0600

    go started

---
+++ python/basic/base.py
@@ -0,0 +1,40 @@
+# * Numeric Data Type
+
+age =65
+# print(type(age))
+
+marks = 86.5
+# print(type(marks))
+
+# * complex data type
+
+c = 5 + 3j
+# print(c)
+# print(type(c))
+
+# * String data type
+
+name = "John"
+# print(name)
+
+# * Boolean data type                                                       
+is_student = True
+# print(is_student)
+# print(type(is_student))
+
+
+# * Sequence data type
+
+# * List data type // array in other programming language 
+# * List is mutable data type and its allow duplicate values 
+citites = ["Kathmandu", "Pokhara", "Biratnagar"]
+numberBool = [1, 2, 3, 4, 5, True, False,"ani",2.5, 3.5, 4.5, 5.5,2]
+# print(numberBool)
+# print(citites)
+# print(type(citites))
+# print(numberBool[2])
+
+
+# * Tuple data type
+
+
